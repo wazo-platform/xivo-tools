fa7fb137a654481ab6dfd93727bfa2bf043c623e
Steven Le Bras <slebras@avencall.com>
2012-12-05 10:43:53 -0500

    Delete load-tester and load-monitor

--- load-tester/loadtester/runner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-
-class ScenarioRunner(object):
-
-    def start_scenario(self, scenario, scenario_config):
-        context = scenario_config.get_context_for_scenario(scenario.name)
-        scenario.prepare_run(context)
-        scenario.run()
--- load-tester/scenarios/call-then-cancel-on-ringing/start.sh.tpl
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sipp -inf users.csv -sf scenario.xml -p {{ bind_port }} {{ sipp_std_options }} {{ sipp_remote_host }}
-
