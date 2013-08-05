c977f110ef393d92b775c932005a35b6a0b656c9
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-08-05 10:44:49 -0400

    reindent script to build debian package

--- dev-tools/buildpkg.sh
+++ dev-tools/buildpkg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#apt-get install git build-essential python-dev debhelper
+
 PKGNAME="$1"
 BRANCH="master"
 GIT_ROOT="git://git.xivo.fr/official"
@@ -7,25 +9,25 @@ GIT_ROOT="git://git.xivo.fr/official"
 [ -n "$2" ] && BRANCH="$2"
 
 function clone_or_pull_repo() {
-	repo="$1"
-	if [ ! -d "$repo" ]; then
-		git clone $GIT_ROOT/$repo.git
-	else
-		cd $repo
+    repo="$1"
+    if [ ! -d "$repo" ]; then
+        git clone $GIT_ROOT/$repo.git
+    else
+        cd $repo
         git fetch -p
-		cd ..
-	fi
+        cd ..
+    fi
 }
 
 function buildpkg() {
-	pkgname="$1"
-	branch="$2"
-	cp -r squeeze-xivo-skaro/$pkgname/trunk/debian $pkgname/$pkgname
-	cd $pkgname/$pkgname
+    pkgname="$1"
+    branch="$2"
+    cp -r squeeze-xivo-skaro/$pkgname/trunk/debian $pkgname/$pkgname
+    cd $pkgname/$pkgname
     git checkout $branch
     git reset --hard origin/$branch
-	dpkg-buildpackage -tc
-	cd ../..
+    dpkg-buildpackage -tc
+    cd ../..
 }
 
 clone_or_pull_repo squeeze-xivo-skaro
