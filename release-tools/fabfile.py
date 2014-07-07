436e340375989bfd37db27cdeec6067a6863bd2e
Sebastien Duthil <sduthil@avencall.com>
2014-07-07 16:02:59 -0400

    release: make copy_binaries faster

--- release-tools/fabfile.py
+++ release-tools/fabfile.py
@@ -92,11 +92,48 @@ def shutdown_slave():
     run('/sbin/poweroff')
 
 
-@hosts(BUILDER_HOST)
 def copy_binaries(version):
     """copy ISO and xivo client debs onto mirror (but not publicly visible)"""
 
-    options = "-v -rlpt --progress --include '/*{version}*' --exclude '/*'".format(version=version)
+    file_names = execute(_get_binaries_file_names, version).get(BUILDER_HOST)
+    execute(_copy_binaries_from_current_version, version, file_names)
+    execute(_copy_binaries_delta, version)
+
+
+def _list_files(path, pattern='*'):
+    command = "find {path} -mindepth 1 -maxdepth 1 -name '{pattern}' -printf '%f\n' | sort".format(path=path,
+                                                                                                   pattern=pattern)
+    return run(command).split('\r\n')
+
+
+@hosts(BUILDER_HOST)
+def _get_binaries_file_names(version):
+    return _list_files('/var/www/builder/', '*{version}*'.format(version=version))
+
+
+@hosts(MIRROR_HOST)
+def _copy_binaries_from_current_version(version, new_file_names):
+    """take ISO from current version and put it in place of the new ISO. This
+    may seem wrong, but rsync will correct it, and the correction will be a lot
+    faster than transferring the whole new ISO file, as only delta will be
+    transferred"""
+
+    new_directory = '/data/iso/archives/.xivo-{version}'.format(version=version)
+    run('cp -rp /data/iso/xivo-current/ {}'.format(new_directory))
+    old_file_names = _list_files(new_directory)
+    for old, new in zip(old_file_names, new_file_names):
+        if old == new:
+            continue
+
+        command = 'mv -f {new_dir}/{old_file} {new_dir}/{new_file}'.format(new_dir=new_directory,
+                                                                           old_file=old,
+                                                                           new_file=new)
+        run(command)
+
+
+@hosts(BUILDER_HOST)
+def _copy_binaries_delta(version):
+    options = "-v -rlpt --delete --progress --include '/*{version}*' --exclude '/*'".format(version=version)
     src = '/var/www/builder/'
     dest = 'www-data@mirror.xivo.io:/data/iso/archives/.xivo-{version}'.format(version=version)
 
@@ -112,7 +149,7 @@ def publish_binaries():
 
     with cd('/data/iso'):
         binaries_path = 'archives'
-        candidates = run("find {path} -maxdepth 1 -name '.xivo-*' -printf '%f\n'".format(path=binaries_path)).split('\r\n')
+        candidates = _list_files(binaries_path, pattern='.xivo-*')
         if not candidates:
             abort('Nothing to publish')
 
