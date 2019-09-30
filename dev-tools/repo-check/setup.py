a6f2a1d781582b0937eb07875f8e41ca6876b1cd
Francois Blackburn <fblackburn@wazo.community>
2019-09-30 13:20:11 -0400

    wazo-pbx -> wazo-platform

--- dev-tools/repo-check/setup.py
+++ dev-tools/repo-check/setup.py
@@ -21,7 +21,7 @@ setup(
     description='XiVO Merge assistant',
     author='Avencall',
     author_email='dev@avencall.com',
-    url='https://github.com/wazo-pbx/xivo-tools',
+    url='https://github.com/wazo-platform/xivo-tools',
     packages=packages,
     scripts=['bin/check_local_xivo_repositories',
              'bin/check_unmerged_branches'],
